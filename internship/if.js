2965b7b9daf732d1d2c8ede95929a3f65015f333
Ramsteyn450 <rr4325812@gmail.com>
2025-12-19 11:52:41 +0530

    Add files via upload

---
+++ internship/if.js
@@ -0,0 +1,11 @@
+let mood = "happy";
+
+if (mood === "happy") {
+    console.log("😊 Keep smiling!");
+} else if (mood === "sad") {
+    console.log("😢 Things will get better!");
+} else if (mood === "angry") {
+    console.log("😡 Take a deep breath...");
+} else {
+    console.log("🤔 Just be yourself!");
+}
